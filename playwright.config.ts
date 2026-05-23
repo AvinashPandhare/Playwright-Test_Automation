ae3afbae38d33632274bba99839048c0045d2376
Avinash <avinashpandhare99@gmail.com>
2026-05-23 19:58:45 +0530

    First Commit-1

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+// Test
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
