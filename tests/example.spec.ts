1261605ca99c016fc5d4486d7ae1f674a97a2909
Avinash <avinashpandhare99@gmail.com>
2026-05-23 20:15:32 +0530

    added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,16 +3,17 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 // CI
+await page.waitForTimeout(30000); // waits for 3 seconds0
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
+await page.waitForTimeout(30000); // waits for 3 seconds
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-
+await page.waitForTimeout(30000); // waits for 3 seconds
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
